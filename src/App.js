c22458cc475a264b56e9d4c6242e01f5c14ec6ed
KayLemba <kaylemba@yahoo.com>
2022-02-01 20:13:12 +0200

    Setup react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import CountryData from './components/CountryData';
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/country/:name" element={<CountryData />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
