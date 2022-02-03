a05c231ddc6b639dc1e41abb71b266a1095f166b
KayLemba <kaylemba@yahoo.com>
2022-02-03 13:35:14 +0200

    Add styling and Unit tests

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { fetchData } from '../redux/covidData/covid';
 
 function Home() {
   const covidReducer = useSelector((state) => state.covidReducer.covid);
   const dispatch = useDispatch();
+  let Africa = covidReducer.filter((item) => item.continent === 'Africa');
+  const loci = useLocation();
+  const query = new URLSearchParams(loci.search);
+  const buscar = query.get('buscar') || '';
+  const navigate = useNavigate();
+  Africa = Africa.filter((country) => country.country.includes(buscar.toLocaleUpperCase()));
+  const [searchInput, setSearchInput] = useState(buscar);
+
   useEffect(() => {
     if (!covidReducer.length) {
       dispatch(fetchData());
     }
   }, []);
-  const Africa = covidReducer.filter((item) => item.continent === 'Africa');
-  return (
-    <div className="body">
 
-      {
+  const Country = (e) => {
+    navigate(e.target.value ? `?buscar=${e.target.value}` : '');
+    setSearchInput(e.target.value);
+  };
+  return (
+    <>
+      <div className="heading">
+        {' '}
+        <h1>AFRICAN COUNTRIES COVID-19 METRICS</h1>
+        {' '}
+      </div>
+      <div className="input-box">
+        <input value={searchInput} onChange={Country} className="input" type="text" placeholder="Search..." />
+      </div>
+      <div className="body">
+        {
         Africa.map((data) => {
           const {
             country,
@@ -26,8 +46,10 @@ function Home() {
             <Link key={country} to={{ pathname: `/country/${country}` }}>
 
               <div className="main-cont" key={country}>
-                {country}
-                <img src={flag} alt={country} />
+                <h2 className="country">{country}</h2>
+                <div className="image">
+                  <img className="img" src={flag} alt={country} />
+                </div>
 
               </div>
             </Link>
@@ -35,7 +57,8 @@ function Home() {
           );
         })
       }
-    </div>
+      </div>
+    </>
 
   );
 }
